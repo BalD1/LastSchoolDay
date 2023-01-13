5940eb4624452f2e8febf557b2af9cab14301b1d
Florian <florianhusson@hotmail.fr>
2023-01-13 18:57:19 +0100

    shirley full anims

--- Too Cliche To Be An Undead/Assets/Graphs/Spine/Characters/Shirley/salut.cs
+++ Too Cliche To Be An Undead/Assets/Graphs/Spine/Characters/Shirley/salut.cs
@@ -7,28 +7,31 @@ public class salut : MonoBehaviour
 {
     [SerializeField] private SkeletonAnimation skeletonAnimation;
 
-    [SerializeField][SpineAnimation] private string idle, walk, dash, death, skill_trans, skill_idle, skill_walk;
+    [SerializeField][SpineAnimation] private string idle, walk, dash, death, skill_trans, skill_idle, skill_walk, attaque_cote, attaque_face, attaque_dos;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F5)) skeletonAnimation.AnimationState.SetAnimation(0, idle, true);
-        if (Input.GetKeyDown(KeyCode.F6)) skeletonAnimation.AnimationState.SetAnimation(0, walk, true);
-        if (Input.GetKeyDown(KeyCode.F7)) skeletonAnimation.AnimationState.SetAnimation(0, dash, true);
-        if (Input.GetKeyDown(KeyCode.F8)) skeletonAnimation.AnimationState.SetAnimation(0, death, false);
-        if (Input.GetKeyDown(KeyCode.F9)) skeletonAnimation.AnimationState.SetAnimation(0, skill_trans, false);
-        if (Input.GetKeyDown(KeyCode.F10)) skeletonAnimation.AnimationState.SetAnimation(0, skill_idle, true);
-        if (Input.GetKeyDown(KeyCode.F11)) skeletonAnimation.AnimationState.SetAnimation(0, skill_walk, true);
-
-        if (Input.GetKeyDown(KeyCode.F12))
+        if (Input.GetKeyDown(KeyCode.F5))
         {
-            skeletonAnimation.AnimationState.SetAnimation(0, skill_trans, false);
-            skeletonAnimation.AnimationState.AddAnimation(0, skill_idle, true, 0);
+            skeletonAnimation.skeleton.SetToSetupPose();
+            Vector2 scale = skeletonAnimation.transform.localScale;
+            scale.x = 1;
+            skeletonAnimation.transform.localScale = scale;
+            skeletonAnimation.AnimationState.SetAnimation(0, attaque_cote, false);
         }
-
-        if (Input.GetKeyDown(KeyCode.F4))
+        if (Input.GetKeyDown(KeyCode.F6))
         {
-            skeletonAnimation.AnimationState.SetAnimation(0, skill_trans, false);
-            skeletonAnimation.AnimationState.AddAnimation(0, skill_walk, true, 0);
+            skeletonAnimation.skeleton.SetToSetupPose();
+            Vector2 scale = skeletonAnimation.transform.localScale;
+            scale.x = -1;
+            skeletonAnimation.transform.localScale = scale;
+            skeletonAnimation.AnimationState.SetAnimation(0, attaque_cote, false);
         }
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            skeletonAnimation.skeleton.SetToSetupPose();
+            skeletonAnimation.AnimationState.SetAnimation(0, attaque_face, false);
+        }
+        if (Input.GetKeyDown(KeyCode.F10)) skeletonAnimation.AnimationState.SetAnimation(0, attaque_dos, false);
     }
 }
