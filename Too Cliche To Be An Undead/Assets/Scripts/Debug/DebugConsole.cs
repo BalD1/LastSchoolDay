54f1471636a7ca29c45d767aaf70acb1d881e45d
Florian <florianhusson@hotmail.fr>
2023-03-12 12:07:32 +0100

    fixed cheat console

--- Too Cliche To Be An Undead/Assets/Scripts/Debug/DebugConsole.cs
+++ Too Cliche To Be An Undead/Assets/Scripts/Debug/DebugConsole.cs
@@ -89,6 +89,8 @@ public class DebugConsole : MonoBehaviour
 
     private bool allowGameChange = true;
 
+    private float labelsHeight = 20;
+
     private void Awake()
     {
         // Create commands
@@ -389,9 +391,7 @@ public class DebugConsole : MonoBehaviour
         if (showConsole)
         {
             HandleInput();
-            input = "";
-            showConsole = false;
-            showHelp = false;
+            ResetField();
 
             if (allowGameChange)
                  GameManager.Instance.GameState = GameManager.E_GameState.InGame;
@@ -403,6 +403,9 @@ public class DebugConsole : MonoBehaviour
         allowGameChange = true;
         showHelp = false;
         input = "";
+        suggestionsScroll = Vector2.zero;
+        currentSelectedSuggestion = 0;
+        GUI.ScrollTo(new Rect(0,0,0,0));
     }
 
     private void HandleInput()
@@ -461,11 +464,8 @@ public class DebugConsole : MonoBehaviour
 
     private bool ParseBool(string propriety)
     {
-        bool res = false;
-
-        if (propriety.Equals("1") || propriety.Equals("TRUE")) res = true;
-
-        return res;
+        if (propriety.Equals("1") || propriety.Equals("TRUE")) return true;
+        return false;
     }
 
     private void OnGUI()
@@ -476,131 +476,162 @@ public class DebugConsole : MonoBehaviour
 
         float y = 0f;
 
+        // Draw the help box field if needed
         Rect helpBoxRect = new Rect(0, y, Screen.width, 100);
-        if (showHelp)
-        {
-            // draw the help box
-            GUI.Box(helpBoxRect, "");
-            Rect viewport = new Rect(0, 0, Screen.width - 30, 20 * commandList.Count);
-            helpScroll = GUI.BeginScrollView(new Rect(0, y + 5f, Screen.width, 90), helpScroll, viewport);
-
-            // show every command with their description and format
-            for (int i = 0; i < commandList.Count; i++)
-            {
-                DebugCommandBase command = commandList[i] as DebugCommandBase;
-
-                string label = $"{command.CommandFormat} - {command.CommandDescription}";
-
-                Rect labelRect = new Rect(5, 20 * i, viewport.width - 100, 20);
-
-                GUI.Label(labelRect, label);
-            }
-
-            GUI.EndScrollView();
-
-            y += 100;
-        }
+        if (showHelp) DrawHelpBox(ref y, ref helpBoxRect);
 
         // draw the input field
-        Rect inputBoxRect = new Rect(0, y, Screen.width, 30);
-        GUI.Box(inputBoxRect, "");
-
-        GUI.backgroundColor = new Color(0, 0, 0, 0);
-        GUI.SetNextControlName("InputField");
-        Rect inputRect = new Rect(10f, y + 5f, Screen.width - 20f, 20f);
-        input = GUI.TextField(inputRect, input);
-        GUI.FocusControl("InputField");
+        Rect inputBoxRect = new Rect(0, y, Screen.width, labelsHeight + 10);
+        DrawInputBox(ref y, ref inputBoxRect);
 
         // will be useful if we need to manually set the selected text
         editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
 
         if (input == "" || input == null) return;
 
-        // automaticaly set the input to upper case
-        if (char.IsLower(input[input.Length - 1]))
-            input = input.ToUpper();
-        
-        // close the console if the player presses the button
-        if (input == "²")
+        // handles the latest input from the user
+        HandleLatestInput();
+
+        DrawSuggestionsBox(y, baseColor);
+    }
+
+    private void DrawHelpBox(ref float y, ref Rect helpBoxRect)
+    {
+        // draw the help box
+        GUI.Box(helpBoxRect, "");
+        Rect viewport = new Rect(0, 0, Screen.width - 30, 20 * commandList.Count);
+        helpScroll = GUI.BeginScrollView(new Rect(0, y + 5f, Screen.width, 90), helpScroll, viewport);
+
+        // show every command with their description and format
+        for (int i = 0; i < commandList.Count; i++)
         {
-            showConsole = false;
-            ResetField();
-            GameManager.E_GameState currentState = GameManager.Instance.GameState;
-            if (currentState == GameManager.E_GameState.Restricted && allowGameChange)
-                GameManager.Instance.GameState = GameManager.E_GameState.InGame;
+            DebugCommandBase command = commandList[i] as DebugCommandBase;
+
+            string label = $"{command.CommandFormat} - {command.CommandDescription}";
+
+            Rect labelRect = new Rect(5, labelsHeight * i, viewport.width - 100, labelsHeight);
+
+            GUI.Label(labelRect, label);
         }
 
-        // Check if the player pressed Quote or Return
-        if (Event.current.isKey)
-        {
-            if (Event.current.keyCode == KeyCode.Return)
-                HandleInput();
-            if (Event.current.keyCode == KeyCode.Quote)
-            {
-                showConsole = false;
-                ResetField();
+        GUI.EndScrollView();
 
-                GameManager.E_GameState currentState = GameManager.Instance.GameState;
-                if (currentState == GameManager.E_GameState.Restricted && allowGameChange)
-                    GameManager.Instance.GameState = GameManager.E_GameState.InGame;
-            }
-        }
-        
+        y += 100;
+    }
 
-        // draw the box
+    private void DrawInputBox(ref float y, ref Rect inputBoxRect)
+    {
+        GUI.Box(inputBoxRect, "");
+
+        GUI.backgroundColor = new Color(0, 0, 0, 0);
+        GUI.SetNextControlName("InputField");
+        Rect inputRect = new Rect(10f, y + 5f, Screen.width - labelsHeight, labelsHeight);
+        input = GUI.TextField(inputRect, input);
+        GUI.FocusControl("InputField");
+
+        y += inputBoxRect.height;
+    }
+
+    private void DrawSuggestionsBox(float y, Color baseColor)
+    {
         GUI.backgroundColor = baseColor;
 
-        float height = y + inputBoxRect.height;
-
-        Rect suggestionsBoxRect = new Rect(0, height, Screen.width, 60);
+        Rect suggestionsBoxRect = new Rect(0, y, Screen.width, 60);
         GUI.Box(suggestionsBoxRect, "");
-
-        Rect suggestionsViewport = new Rect(0, height, Screen.width - 30, 20 * commandList.Count);
-        suggestionsScroll = GUI.BeginScrollView(new Rect(0, height + 5f, Screen.width, 50), suggestionsScroll, suggestionsViewport);
 
         suggestions = new List<string>();
         suggestionsRect = new List<Rect>();
 
+        // iterates through every commands
         for (int i = 0; i < commandList.Count; i++)
         {
+            // if the current input starts with the i command ID, add it to the suggestions
             DebugCommandBase command = commandList[i] as DebugCommandBase;
             if (command.CommandID.StartsWith(input) == false) continue;
 
             suggestions.Add(command.CommandFormat);
-            suggestionsRect.Add(new Rect(5, (y + suggestionsRect.Count) + 20 * (i + 1), suggestionsViewport.width - 100, 20));
+
+            Vector2 suggestionPos = new Vector2(
+                x: 5,
+                y: (y + suggestionsRect.Count) + labelsHeight * (i + 1)
+                );
+
+            Rect suggestionRect = new Rect(
+                x: suggestionPos.x,
+                y: suggestionPos.y,
+                width: Screen.width - 130,
+                height: labelsHeight
+                );
+
+            suggestionsRect.Add(suggestionRect);
         }
+
+        Rect suggestionsViewport = new Rect(0, y, Screen.width - 30, labelsHeight * suggestions.Count);
+        Rect suggestionScrollRect = new Rect(0, y + 5f, Screen.width, 50);
+        suggestionsScroll = GUI.BeginScrollView(suggestionScrollRect, suggestionsScroll, suggestionsViewport);
 
         for (int i = 0; i < suggestions.Count; i++)
         {
             string label = suggestions[i];
 
-            Rect labelRect = new Rect(5, y + 20 * (i + 2), suggestionsViewport.width - 100, 20);
+            Vector2 suggestionPos = new Vector2(
+                x: 5,
+                y: y + labelsHeight * i
+                );
+
+            Rect labelRect = new Rect(suggestionPos.x, suggestionPos.y, Screen.width - 130, labelsHeight);
 
             if (currentSelectedSuggestion == i)
                 GUI.Label(labelRect, label, suggestionSkin);
             else
                 GUI.Label(labelRect, label);
         }
-        ChoseSuggestion(height);
+        ChooseSuggestion(y);
 
         GUI.EndScrollView();
     }
 
-    private void ChoseSuggestion(float height)
+    private void HandleLatestInput()
+    {
+        // automaticaly set the input to upper case
+        if (char.IsLower(input[input.Length - 1]))
+            input = input.ToUpper();
+
+        // close the console if the player presses the button
+        if (input == "²") OnToggleConsole();
+
+        // Check if the player pressed Quote or Return
+        if (Event.current.isKey)
+        {
+            if (Event.current.keyCode == KeyCode.Return) OnReturn();
+            if (Event.current.keyCode == KeyCode.Quote) OnToggleConsole();
+        }
+    }
+
+    private void ChooseSuggestion(float height)
     {
 
         if (Event.current.isKey && Event.current.keyCode == KeyCode.DownArrow)
         {
             currentSelectedSuggestion += 1;
-            if (currentSelectedSuggestion > suggestions.Count - 1) currentSelectedSuggestion = 0;
-            GUI.ScrollTo(suggestionsRect[currentSelectedSuggestion]);
+            if (currentSelectedSuggestion > suggestions.Count - 1)
+            {
+                currentSelectedSuggestion = 0;
+                suggestionsScroll.y = 0;
+            }
+            else suggestionsScroll.y += labelsHeight;
         }
 
         if (Event.current.isKey && Event.current.keyCode == KeyCode.UpArrow)
         {
             currentSelectedSuggestion -= 1;
-            if (currentSelectedSuggestion < 0) currentSelectedSuggestion = suggestions.Count - 1;
-            GUI.ScrollTo(suggestionsRect[currentSelectedSuggestion]);
+            if (currentSelectedSuggestion < 0)
+            {
+                currentSelectedSuggestion = suggestions.Count - 1;
+                suggestionsScroll.y = (suggestions.Count - 1) * labelsHeight;
+            }
+            else suggestionsScroll.y -= labelsHeight;
+            //GUI.ScrollTo(suggestionsRect[currentSelectedSuggestion]);
         }
 
 
