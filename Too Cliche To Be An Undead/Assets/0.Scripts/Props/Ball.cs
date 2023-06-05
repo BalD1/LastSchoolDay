28d2649612efb6abef5eb9f7e9a5ea65bc590b38
Florian <florianhusson@hotmail.fr>
2023-06-05 18:43:52 +0200

    [ADDED] [FIXES]
    - Fixed tables audio not playing correctly
    - Added ball audio

--- Too Cliche To Be An Undead/Assets/0.Scripts/Props/Ball.cs
+++ Too Cliche To Be An Undead/Assets/0.Scripts/Props/Ball.cs
@@ -7,6 +7,11 @@ public class Ball : MonoBehaviour, IDamageable
     [SerializeField] private float speedMultiplier;
     [SerializeField] private Rigidbody2D body;
 
+    [SerializeField] private AudioSource source;
+    [SerializeField] private SCRPT_DestroyablePropsAudio audioData;
+
+    [SerializeField] private float velocityMagnitudeThresholdForAudio = .5f;
+
     public bool IsAlive() => true;
 
     public void OnDeath(bool forceDeath = false)
@@ -20,9 +25,21 @@ public class Ball : MonoBehaviour, IDamageable
     public bool OnTakeDamages(float amount, Entity damager, bool isCrit = false, bool fakeDamages = false, bool callDelegate = true, bool tickDamages = false)
     {
         Vector2 dir = this.transform.position - damager.transform.position;
-
         body.AddForce(dir * amount * speedMultiplier, ForceMode2D.Impulse);
+        PlayAudio();
 
         return true;
+    }
+
+    private void PlayAudio()
+    {
+        source.pitch = Random.Range(.9f, 1.1f);
+        source.PlayOneShot(audioData.HurtClips.RandomElement());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (body.velocity.magnitude >= velocityMagnitudeThresholdForAudio)
+            PlayAudio();
     }
 }
